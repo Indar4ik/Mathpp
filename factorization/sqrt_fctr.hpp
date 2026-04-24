a2a264a17576d08d8719c1467ceac21df2db316b
Indar4ik <pozdnovcube@gmail.com>
2026-04-24 09:21:32 +0300

    modified:   factorization/sqrt_fctr.hpp

--- factorization/sqrt_fctr.hpp
+++ factorization/sqrt_fctr.hpp
@@ -1,22 +1,29 @@
 #pragma once
 #include <vector>
 #include <bit>
+#include <cstdint>
 
-inline std::vector<unsigned long> sqrt_fctr(unsigned long n) noexcept{
-    if (n <= 1) return {};
-    [[assume(n > 1)]];
+inline void sqrt_fctr(unsigned long n, std::vector<unsigned long>& factors) noexcept{
+    factors.clear();
+    if (n <= 1) return;
+
     int twos = std::countr_zero(n);
     n >>= twos;
-    std::vector<unsigned long> factors(twos, 2UL);
+    for (int i = 0; i < twos; ++i) factors.push_back(2UL);
     // factors.reserve(64);
 
     for (unsigned long i = 3; i * i <= n; i += 2){
-        while (n % i == 0){
+        // while (n % i == 0){
+        //     factors.push_back(i);
+        //     n /= i;
+        // }
+        while (true){
+            unsigned long q = n / i;
+            unsigned long r = n % i;
+            if (r != 0) break;
             factors.push_back(i);
-            n /= i;
+            n = q;
         }
     }
     if (n > 1) factors.push_back(n);
-    
-    return factors;
 }
