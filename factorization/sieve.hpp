9128bddd84795a3fba645eea0792effdbd5641ac
Indar4ik <pozdnovcube@gmail.com>
2026-04-24 09:36:59 +0300

    modified:   .gitignore 	modified:   factorization/sieve.hpp

--- factorization/sieve.hpp
+++ factorization/sieve.hpp
@@ -2,20 +2,20 @@
 #include <vector>
 #include <cmath>
 
-inline std::vector<int> sieve(int n) noexcept{
-    std::vector<int> lp(n + 1, 0), primes;
+inline std::vector<unsigned long> sieve(unsigned long n) noexcept{
+    std::vector<unsigned long> lp(n + 1, 0), primes;
     primes.reserve(n / log(n));
 
-    for (int i = 2; i <= n; ++i){
+    for (unsigned long i = 2; i <= n; ++i){
         if (lp[i] == 0){
             lp[i] = i;
             primes.push_back(i);
         }
-        for (const int& p : primes){
+        for (const unsigned long& p : primes){
             if (p > lp[i] || p > n / i) break;
             lp[p * i] = p;
         }
     }
 
-    return lp;
+    return primes;
 }
