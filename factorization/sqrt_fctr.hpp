bbdd708031b90ad840eb05ac93efcee55a5e20ab
Indar4ik <pozdnovcube@gmail.com>
2026-04-24 08:46:56 +0300

    modified:   factorization/sqrt_fctr.hpp

--- factorization/sqrt_fctr.hpp
+++ factorization/sqrt_fctr.hpp
@@ -1,15 +1,16 @@
 #pragma once
-#include <algorithm>
 #include <vector>
-#include <cmath>
 #include <bit>
 
 inline std::vector<unsigned long> sqrt_fctr(unsigned long n) noexcept{
-    std::vector<unsigned long> factors;
-    factors.reserve(n / log(n));
+    if (n > 0) return {};
+    [[assume(n > 0)]];
+    int twos = std::countr_zero(n);
+    n >>= twos;
+    std::vector<unsigned long> factors(twos, 2UL);
+    // factors.reserve(64);
 
-    std::fill_n(factors.data(), std::countr_zero(n), 2L);
-    for (int i = 3; i * i <= n; i += 2){
+    for (unsigned long i = 3; i * i <= n; i += 2){
         while (n % i == 0){
             factors.push_back(i);
             n /= i;
