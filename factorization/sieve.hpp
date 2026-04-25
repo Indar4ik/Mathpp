d7b31c27897cea86c6036998bd88d5865c7732f5
Indar4ik <pozdnovcube@gmail.com>
2026-04-25 11:03:47 +0300

    modified:   factorization/sieve.hpp

--- factorization/sieve.hpp
+++ factorization/sieve.hpp
@@ -25,35 +25,28 @@ inline std::vector<unsigned> sieve(unsigned n) noexcept{
 }
 
 inline std::vector<unsigned> firstNprimes(unsigned n) noexcept{
-    [[assume(n>0)]];
+    [[assume(n > 0)]];
     if (n == 1) return {2};
-    if (n == 2) return {2, 3};
-    if (n == 3) return {2, 3, 5};
 
-    size_t k = 3;
+    size_t k = 1;
     unsigned limit = (n < 6) ? 13 : (unsigned)(n * (log(n) + log(log(n))));
-    std::vector<unsigned> lp(limit + 1, 0), primes(n);
+    std::vector<unsigned> lp(limit / 2+ 1, 0), primes(n);
 
-    primes[0] = 2; primes[1] = 3; primes[2] = 5;
-    
-    static const unsigned increments[] = {6, 4, 2, 4, 2, 4, 6, 2};
-    unsigned i = 7, idx = 0;
+    primes[0] = 2;
 
-    while (i <= limit){
-        if (lp[i] == 0){
-            lp[i] = i;
+    for (unsigned i = 3; i <= limit; i += 2){
+        unsigned idx = i / 2;
+        if (lp[idx] == 0){
+            lp[idx] = i;
             primes[k++] = i;
             if (k == n) break;
         }
 
-        unsigned max_p = std::min(limit / i, lp[i]);
-        for (unsigned j = 0; j < k; ++j){
+        unsigned max_p = std::min(limit / i, lp[idx]);
+        for (unsigned j = 1; j < k; ++j){
             if (primes[j] > max_p) break;
-            lp[primes[j] * j] = primes[j];
+            lp[primes[j] * i / 2] = primes[j];
         }
-
-        i += increments[idx];
-        idx = (idx + 1) & 7;
     }
 
     return primes;
