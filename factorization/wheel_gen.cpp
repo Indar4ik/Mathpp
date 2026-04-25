58e52afd395c1001ff39e5ea3d893dd5f9bc6b17
Indar4ik <pozdnovcube@gmail.com>
2026-04-25 15:44:37 +0300

    Генератор офсетов для wheel factorization

---
+++ factorization/wheel_gen.cpp
@@ -0,0 +1,25 @@
+#include "sieve.hpp"
+#include <iostream>
+#include <print>
+
+int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    unsigned int n, p = 1;
+    std::cin >> n; // Количество чисел в колесе
+
+    for (const unsigned& prime : firstNprimes(n)){
+        p *= prime;
+    }
+
+    std::vector<unsigned> primes_to_p = sieve(p + 1);
+
+    std::print("{{");
+    for (unsigned i = n + 1; i < primes_to_p.size(); ++i){
+        std::print("{}, ", primes_to_p[i] - primes_to_p[i - 1]);
+    }
+    std::println("{}}}", p + primes_to_p[n] - primes_to_p.back());
+    std::println("i = {}", primes_to_p[n]);
+    std::println("idx = (idx + 1) % {}", primes_to_p.size() - n);
+}
