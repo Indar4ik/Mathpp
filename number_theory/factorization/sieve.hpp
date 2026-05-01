d00fa1db84b0c9a91dcfc50ab90963007e13f4d8
Indar4ik <pozdnovcube@gmail.com>
2026-05-01 18:54:33 +0300

    Немнога bugfix

--- number_theory/factorization/sieve.hpp
+++ number_theory/factorization/sieve.hpp
@@ -3,13 +3,14 @@
 #include <cmath>
 #include <cstdint>
 #include <vector>
+#include <ranges>
 
 inline std::vector<uint32_t> sieve(uint32_t n) noexcept {
-    [[assume(n>0)]];
-    std::vector<uint32_t> lp((n >> 1) + 1, 0), primes;
+    [[assume(n > 0)]];
+    std::vector<uint32_t> lp((n >> 1) + 1, 0), primes = {2};
     primes.reserve(n / log(n));
 
-    for (uint32_t i = 2; i <= n; ++i){
+    for (uint32_t i = 3; i <= n; i += 2){
         uint32_t idx = i >> 1;
         if (lp[idx] == 0){
             lp[idx] = i;
@@ -17,7 +18,7 @@ inline std::vector<uint32_t> sieve(uint32_t n) noexcept {
         }
 
         uint32_t max_p = std::min(n / i, lp[idx]);
-        for (uint32_t p : primes){
+        for (const uint32_t& p : primes | std::views::drop(1)){
             if (p > max_p) break;
             lp[p * i >> 1] = p;
         }
