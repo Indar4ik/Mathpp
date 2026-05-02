53b3b55f4c09c92f77c7212824cc732cccdd2768
Indar4ik <pozdnovcube@gmail.com>
2026-05-02 10:20:54 +0300

    Колесо 2-3 в функциях sieve.hpp

--- number_theory/factorization/sieve.hpp
+++ number_theory/factorization/sieve.hpp
@@ -7,21 +7,24 @@
 
 inline std::vector<uint32_t> sieve(uint32_t n) noexcept {
     [[assume(n > 0)]];
-    std::vector<uint32_t> lp((n >> 1) + 1, 0), primes = {2};
+    std::vector<uint32_t> lp((n >> 1) + 1, 0), primes = {2, 3};
     primes.reserve(n / log(n));
 
-    for (uint32_t i = 3; i <= n; i += 2){
-        uint32_t idx = i >> 1;
+    uint32_t i = 5;
+    const uint32_t max_idx = ((n - 1) & ~1u) / 3;
+    for (uint32_t idx = 1; idx <= max_idx; ++idx){
         if (lp[idx] == 0){
             lp[idx] = i;
             primes.push_back(i);
         }
 
         uint32_t max_p = std::min(n / i, lp[idx]);
-        for (const uint32_t& p : primes | std::views::drop(1)){
+        for (const uint32_t& p : primes | std::views::drop(2)){
             if (p > max_p) break;
-            lp[p * i >> 1] = p;
+            lp[p * i / 3] = p;
         }
+
+        i += 6 - ((i % 3) << 1);
     }
 
     return primes;
@@ -31,14 +34,15 @@ inline std::vector<uint32_t> firstNprimes(uint32_t n) noexcept {
     [[assume(n > 0)]];
     if (n == 1) return {2};
 
-    size_t k = 1;
+    size_t k = 2;
     uint32_t limit = (n < 6) ? 13 : (uint32_t)(n * (log(n) + log(log(n))));
     std::vector<uint32_t> lp((limit >> 1) + 1, 0), primes(n);
 
-    primes[0] = 2;
+    primes[0] = 2; primes[1] = 3;
 
-    for (uint32_t i = 3; i <= limit; i += 2){
-        uint32_t idx = i >> 1;
+    uint32_t i = 5;
+    const uint32_t max_idx = ((limit - 1) & ~1u) / 3;
+    for (uint32_t idx = 1; idx <= max_idx; ++idx){
         if (lp[idx] == 0){
             lp[idx] = i;
             primes[k++] = i;
@@ -46,10 +50,12 @@ inline std::vector<uint32_t> firstNprimes(uint32_t n) noexcept {
         }
 
         uint32_t max_p = std::min(limit / i, lp[idx]);
-        for (uint32_t j = 1; j < k; ++j){
-            if (primes[j] > max_p) break;
-            lp[primes[j] * i >> 1] = primes[j];
+        for (const uint32_t& p : primes | std::views::drop(2) | std::views::take(k - 2)){
+            if (p > max_p) break;
+            lp[p * i / 3] = p;
         }
+
+        i += 6 - ((i % 3) << 1);
     }
 
     return primes;
