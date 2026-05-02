b155caec830cfe88f5ffcbfa63c757d98cd16ebf
Indar4ik <pozdnovcube@gmail.com>
2026-05-02 12:00:01 +0300

    Колесо 2-3-5 в функциях sieve.hpp

--- number_theory/factorization/sieve.hpp
+++ number_theory/factorization/sieve.hpp
@@ -5,44 +5,66 @@
 #include <vector>
 #include <ranges>
 
+inline uint32_t idx_to_x (uint32_t idx) noexcept {
+    return (0x62642424U >> ((idx & 7) << 2)) & 15;
+}
+
+inline uint32_t x_to_idx (uint32_t x) noexcept {
+    uint32_t x_offset = x - 7;
+
+    uint32_t k = x_offset / 30;
+    uint32_t rem = x_offset % 30;
+
+    uint32_t r = (0x777655544322100ULL >> ((rem >> 1) << 2)) & 15;
+    return (k << 3) | r;
+}
+
 inline std::vector<uint32_t> sieve(uint32_t n) noexcept {
     [[assume(n > 0)]];
-    std::vector<uint32_t> lp((n >> 1) + 1, 0), primes = {2, 3};
+    if (n < 2) return {};
+    if (n < 3) return {2};
+    if (n < 5) return {2, 3};
+    if (n < 7) return {2, 3, 5};
+
+    const uint32_t max_idx = x_to_idx(n);
+    std::vector<uint32_t> lp(max_idx + 1, 0), primes = {2, 3, 5};
     primes.reserve(n / log(n));
 
-    uint32_t i = 5;
-    const uint32_t max_idx = ((n - 1) & ~1u) / 3;
-    for (uint32_t idx = 1; idx <= max_idx; ++idx){
+    uint32_t i = 7;
+    for (uint32_t idx = 0; idx <= max_idx; ++idx){
         if (lp[idx] == 0){
             lp[idx] = i;
             primes.push_back(i);
         }
 
         uint32_t max_p = std::min(n / i, lp[idx]);
-        for (const uint32_t& p : primes | std::views::drop(2)){
+        for (const uint32_t& p : primes | std::views::drop(3)){
             if (p > max_p) break;
-            lp[p * i / 3] = p;
+            lp[x_to_idx(p * i)] = p;
         }
 
-        i += 6 - ((i % 3) << 1);
+        i += idx_to_x(idx);
     }
 
     return primes;
 }
 
 inline std::vector<uint32_t> firstNprimes(uint32_t n) noexcept {
-    [[assume(n > 0)]];
+    if (n == 0) return {};
     if (n == 1) return {2};
+    if (n == 2) return {2, 3};
+    if (n == 3) return {2, 3, 5};
 
-    size_t k = 2;
+    size_t k = 3;
     uint32_t limit = (n < 6) ? 13 : (uint32_t)(n * (log(n) + log(log(n))));
-    std::vector<uint32_t> lp((limit >> 1) + 1, 0), primes(n);
+    const uint32_t max_idx = x_to_idx(limit);
 
-    primes[0] = 2; primes[1] = 3;
+    std::vector<uint32_t> lp(max_idx + 1, 0), primes(n);
 
-    uint32_t i = 5;
-    const uint32_t max_idx = ((limit - 1) & ~1u) / 3;
-    for (uint32_t idx = 1; idx <= max_idx; ++idx){
+    primes[0] = 2; primes[1] = 3; primes[2] = 5;
+
+    uint32_t i = 7;
+    for (uint32_t idx = 0; idx <= max_idx; ++idx){
         if (lp[idx] == 0){
             lp[idx] = i;
             primes[k++] = i;
@@ -50,12 +72,12 @@ inline std::vector<uint32_t> firstNprimes(uint32_t n) noexcept {
         }
 
         uint32_t max_p = std::min(limit / i, lp[idx]);
-        for (const uint32_t& p : primes | std::views::drop(2) | std::views::take(k - 2)){
+        for (const uint32_t& p : primes | std::views::drop(3) | std::views::take(k - 3)){
             if (p > max_p) break;
-            lp[p * i / 3] = p;
+            lp[x_to_idx(i * p)] = p;
         }
 
-        i += 6 - ((i % 3) << 1);
+        i += idx_to_x(idx);
     }
 
     return primes;
