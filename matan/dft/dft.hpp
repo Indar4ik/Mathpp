80554509a979d19cc3262cbb537ee4e52a2fe058
Indar4ik <pozdnovcube@gmail.com>
2026-06-02 18:20:25 +0300

    Подготовительные вычисления вынесены в отдельную фукнцию prepare_fft(). Это вычисление бит-обратных индексов и всех twiddle В fft() добавлен пилинг циклов для лучшей векторизации.

--- matan/dft/dft.hpp
+++ matan/dft/dft.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include <cmath>
+// #include <cmath>
 #include <complex>
 #include <cstddef>
 // #include <mkl.h>
@@ -7,84 +7,65 @@
 #include <utility>
 #include <vector>
 
-// Рекурсивный Кули-Тьюки O(nlogn)
-// void fft(std::vector<std::complex<double>>& arr){
-//     const size_t n = arr.size();
-//     if (n <= 1) return;
-
-//     std::vector<std::complex<double>> even(n / 2), odd(n / 2);
+inline void prepare_fft(size_t& n, std::vector<size_t>& revers_idx, std::vector<std::complex<double>>& twiddles){
+    [[assume((n & (n - 1)) == 0)]];
+    [[assume(n >= 2)]];
     
-//     #pragma clang loop vectorize(enable)
-//     for (size_t i = 0; i < n / 2; ++i){
-//         even[i] = arr[2 * i];
-//         odd[i] = arr[2 * i + 1];
-//     }
+    revers_idx.resize(n, 0);
+    twiddles.resize(n / 2);
 
-//     fft(even);
-//     fft(odd);
-
-//     const double twiddle_step = -2.0 * std::numbers::pi / n; 
-//     for (size_t i = 0; i < n / 2; ++i){
-//         const double twiddle = twiddle_step * i;
-//         const std::complex<double> w(std::cos(twiddle), std::sin(twiddle));
-
-//         const std::complex<double> t = odd[i] * w;
-
-//         arr[i] = even[i] + t;
-//         arr[i + n / 2] = even[i] - t;
-//     }
-// }
-
-// Итеративный Кули-Тьюки O(nlogn)
-inline void fft_iter(std::vector<std::complex<double>>& arr){
-    const size_t n = arr.size();
-    if (n <= 1) return;
-
-    // Бит-реверсная перестановка
     for (size_t i = 1, j = 0; i < n; ++i){
         size_t bit = n >> 1;
         for (; j & bit; bit >>= 1) j ^= bit;
         j ^= bit;
-        if (i < j) std::swap(arr[i], arr[j]);
+        revers_idx[i] = j;
+    }
+    
+    for (size_t i = 0; i < n / 2; ++i) twiddles[i] = {std::cos(2.0 * std::numbers::pi * i / n), -std::sin(2.0 * std::numbers::pi * i / n)};
+}
+
+// Итеративный Кули-Тьюки O(nlogn)
+inline void fft(std::vector<std::complex<double>>& arr, const std::vector<size_t>& revers_idx, const std::vector<std::complex<double>>& twiddles) noexcept {
+    const size_t n = arr.size();
+    if (n <= 1) return;
+    [[assume((n & (n - 1)) == 0)]];
+    [[assume(n >= 2)]];
+    [[assume(revers_idx.size() == n)]];
+    [[assume(twiddles.size() == n / 2)]];
+
+    // Бит-реверсная перестановка
+    for (size_t i = 0; i < n; ++i) if (i < revers_idx[i]) std::swap(arr[i], arr[revers_idx[i]]);
+
+    // Вырожденный случай - бабочки длины 2
+    for (size_t i = 0; i < n; i += 2){
+        std::complex<double> u = arr[i];
+        std::complex<double> v = arr[i + 1];
+        arr[i] = u + v; arr[i + 1] = u - v;
     }
 
-    std::vector<std::complex<double>> twiddle_table(n / 2);
-    for (size_t i = 0; i < n / 2; ++i){
-        twiddle_table[i] = std::polar(1.0, -2.0 * std::numbers::pi * i / n);
+    // Вырожденный случай - бабочки длины 4
+    for (size_t i = 0; i < n; i += 4){
+        std::complex<double> u1 = arr[i];
+        std::complex<double> u2 = arr[i + 1];
+        std::complex<double> v1 = arr[i + 2];
+        std::complex<double> v2 = {arr[i + 3].imag(), -arr[i + 3].real()};
+        arr[i] = u1 + v1; arr[i + 1] = u2 + v2; 
+        arr[i + 2] = u1 - v1; arr[i + 3] = u2 - v2; 
     }
 
-    // Бабочки по каждой паре с увеличивающейся длинной 2 -> 4 -> 8...
-    for (size_t len = 2; len <= n; len <<= 1){
+    // Бабочки по каждой паре с увеличивающейся длинной 8 -> 16 -> 32...
+    for (size_t len = 8; len <= n; len <<= 1){
         for (size_t i = 0; i < n; i += len){
-            std::complex<double> w(1.0, 0.0);
-            for (size_t j = 0; j < len / 2; ++j){
-                std::complex<double> w = twiddle_table[j * (n / len)];
+            std::complex<double> u = arr[i], v = arr[i + len / 2];
+            arr[i] = u + v;
+            arr[i + len / 2] = u - v;
+            #pragma clang loop vectorize(enable) interleave(enable)
+            for (size_t j = 1; j < len / 2; ++j){
+                std::complex<double> w = twiddles[j * (n / len)];
                 std::complex<double> u = arr[i + j], v = arr[i + j + len / 2] * w;
                 arr[i + j] = u + v;
                 arr[i + j + len / 2] = u - v;
             }
         }
     }
-}
-
-// DFT в лоб за O(n^2)
-inline std::vector<std::complex<double>> dft(std::vector<std::complex<double>>& arr){
-    const size_t n = arr.size();
-    if (n <= 1) return arr;
-
-    std::vector<std::complex<double>> ft(n);
-    const double ang_len = 2 * std::numbers::pi / n;
-    double ang = 0.0;
-    for (size_t i = 0; i < n; ++i){
-        std::complex<double> wlen(std::cos(ang), -std::sin(ang));
-        std::complex<double> s(0.0, 0.0), w(1.0, 0.0);
-        for (size_t j = 0; j < n; ++j){
-            s += arr[j] * w; 
-            w *= wlen;
-        }
-        ft[i] = s;
-        ang += ang_len;
-    }
-
-    return ft;
 }
